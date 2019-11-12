01c433047d3125e1967c200bc40d023fb6744e8c
Tacey Wong <xinyong.wang@qq.com>
2019-11-13 02:27:24 +0800

    add swagger

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/go-boy/change_avatar
 go 1.13
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/bitly/go-simplejson v0.5.0
 	github.com/esimov/pigo v1.3.0 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.1.11
 	github.com/stretchr/testify v1.4.0
+	github.com/swaggo/echo-swagger v0.0.0-20190329130007-1219b460a043 // indirect
+	github.com/swaggo/swag v1.6.3
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 )
--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/swaggo/echo-swagger"
+
+	_ "github.com/go-boy/change_avatar/docs"
 )
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
 func main(){
 	hostPort := 8080
 	e := echo.New()
 	e.HideBanner = true
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	setupRouter(e)
 	e.Logger.Info(" Listen and Server in ", hostPort)
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", hostPort)))
